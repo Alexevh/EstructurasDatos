57e9c9bd22f7a521cbe29c65e8709a190389aa5d
Alex <alex2005.uy@gmail.com>
2017-11-05 22:57:25 -0300

    Se agrega el TAD Cola

---
+++ EstructuraDatos/src/Cola/Cola.java
@@ -0,0 +1,62 @@
+/*
+ * Implementacion de colas
+ */
+package Cola;
+
+/**
+ *
+ * @author alex
+ */
+public class Cola {
+    
+    NodoCola inicio;
+    NodoCola fin;
+    int tamano;
+    
+    public Cola()
+    {
+        inicio = null;
+        fin=null;
+        tamano =0;
+    }
+    
+    public boolean estaVacia()
+    {
+        /* Si inicio es null esta vacia regresa true */
+        return inicio==null;
+    }
+    
+    public void insertar(int ele)
+    {
+        NodoCola nuevo = new NodoCola(ele);
+        if (estaVacia())
+        {
+            inicio=nuevo;
+        } else {
+            fin.siguiente=nuevo;
+        }
+        
+        /* El fin siempre apunta al nuevo */
+        fin = nuevo;
+        tamano++;
+    }
+    
+    public NodoCola quitar()
+    {
+        NodoCola  aux = inicio;
+        inicio = inicio.siguiente;
+        tamano--;
+        return aux;
+        
+    }
+    
+    public NodoCola inicoCola()
+    {
+        return inicio;
+    }
+    
+    public int tamanoCola()
+    {
+        return tamano;
+    }
+}
---
+++ EstructuraDatos/src/Cola/NodoCola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cola;
+
+/**
+ *
+ * @author alex
+ */
+public class NodoCola {
+    
+    int dato;
+    NodoCola siguiente;
+    
+    public NodoCola(int i)
+    {
+        this.dato=i;
+        this.siguiente =null;
+    }
+            
+}
